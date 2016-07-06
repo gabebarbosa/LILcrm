c83c036cc3150e1d83b607a89695fcd5e8d0b2f7
gabebarbosa <gabriel.lemosbarbosa@gmail.com>
2016-07-06 09:33:50 -0300

    ajuste navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
                           redirect_to @user, notice: "Usuário foi criado com sucesso!"
                           #tire o método de comentário quando criar o helper.
                           #Usuário depois de cadastrar-se acessa o sistema automaticamente
-  						  #sign_in(@user)
+  						  sign_in(@user)
               else 
   				render action: :new
               end
