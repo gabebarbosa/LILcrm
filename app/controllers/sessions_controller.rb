f5730737060ff0b03fe3ed0b6f786fb686fd5013
Gabriel Barbosa <gabriel.lemosbarbosa@gmail.com>
2016-10-08 18:41:58 +0000

    lay padrão em todas as páginas

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@
       @user = User.find_by(email: params[:session][:email].downcase)
       if @user && @user.authenticate(params[:session][:password])
                  sign_in(@user)
-                 redirect_to current_user
+                 #redirect_to current_user
+                 redirect_to atendimentos_url
       else
        render action: :new
       end
